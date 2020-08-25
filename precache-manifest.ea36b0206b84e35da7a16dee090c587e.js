520fdfe04f6992d3a51bd3bd6948072d9079d1e6
Shein92 <almashij92@gmail.com>
2020-08-25 16:45:43 +0300

    Updates

---
+++ precache-manifest.ea36b0206b84e35da7a16dee090c587e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8f49537d2419f551e86ba43ff248333",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1c132ca16f978096e5ce",
+    "url": "/portfolio/static/css/main.cd6e2d1a.chunk.css"
+  },
+  {
+    "revision": "14b7509c27abad025efe",
+    "url": "/portfolio/static/js/2.9a7e1d97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.9a7e1d97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c132ca16f978096e5ce",
+    "url": "/portfolio/static/js/main.40372659.chunk.js"
+  },
+  {
+    "revision": "298e2d356be8681dde49",
+    "url": "/portfolio/static/js/runtime-main.e4f76399.js"
+  }
+]);
