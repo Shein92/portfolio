432d535984b046d2c0b9265fd4c511507c073035
Shein92 <almashij92@gmail.com>
2020-09-30 17:36:44 +0300

    small fixes

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -1,22 +1,47 @@
 import React from 'react';
 import style from './Footer.module.scss';
 import styleContainer from '../common/styles/Container.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram, faLinkedin, IconDefinition } from '@fortawesome/free-brands-svg-icons';
+import { faBeer } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
-    return (
-        <div className={style.footer}>
-            <div className={`${styleContainer.container} ${style.container}`}>
-                <h3>Vasyl Almashiy</h3>
-                <ul>
-                    <li><a href="#LI" className={style.a}>LinkedIn</a></li>
-                    <li><a href="#FB">FaceBook</a></li>
-                    <li><a href="#INSTA">Instagramm</a></li>
-                    <li><a href="#PUB">My favorite pub</a></li>
-                </ul>
-                <span>© 2020 Все права защищены</span>
-            </div>
-        </div>
-    )
+
+	const LinkedInIcon: IconDefinition = faLinkedin;
+	const FaceBookIcon: IconDefinition = faFacebook;
+	const InstagramIcon: IconDefinition = faInstagram;
+	const PubIcon: IconDefinition = faBeer;
+
+	return (
+		<div className={style.footer}>
+			<div className={`${styleContainer.container} ${style.container}`}>
+				<h3>Vasyl Almashiy</h3>
+				<ul>
+					<li>
+						<a href="#LI">
+							<FontAwesomeIcon className={style.icon} icon={LinkedInIcon} />
+						</a>
+					</li>
+					<li>
+						<a href="#FB">
+							<FontAwesomeIcon className={style.icon} icon={FaceBookIcon} />
+						</a>
+					</li>
+					<li>
+						<a href="#INSTA">
+							<FontAwesomeIcon className={style.icon} icon={InstagramIcon} />
+						</a>
+					</li>
+					<li>
+						<a href="#PUB">
+							<FontAwesomeIcon className={style.icon} icon={PubIcon} />
+						</a>
+					</li>
+				</ul>
+				<span>© 2020 Все права защищены</span>
+			</div>
+		</div>
+	)
 }
 
 export default Footer;
